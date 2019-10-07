f28c63db6088ebef428385c5201b833ea48a75f5
Alice-Ermakova <alesya.ermakova91@gmail.com>
2019-10-07 19:05:44 +0300

    feat: add task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,32 @@
 module.exports = function multiply(first, second) {
-  // your solution
+
+ 
+  //create new variables
+  let multiply = 0;
+  let result = 0;
+
+  //use the global function BigInt to convert strings(first, second) into a number
+  let NumFirst = BigInt(first);
+  let NumSecond = BigInt(second);
+
+  //checking the numbers obtained
+  //console.log(NumFirst);
+  //console.log(NumSecond);
+
+  //multiply two numbers 
+  multiply= NumFirst * NumSecond;
+
+  //checking the result of multiplying numbers
+  //console.log(multiply);
+
+  //checking typeof multiply
+  //console.log(typeof(multiply));
+
+  //BigInt to convert into a string
+  result = multiply.toString();
+
+  //checking the result
+  //console.log(result);
+
+  return result;
 }
